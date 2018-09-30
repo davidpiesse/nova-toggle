f02aa02ef4f91c0317c6ba65f9228776796ed920
David Piesse <piesse@gmail.com>
2018-09-30 21:49:09 +0100

    Show Labels in Index if Set

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -3,11 +3,27 @@
         <span
             class="inline-block rounded-full w-2 h-2"
             :class="{'bg-success': field.value, 'bg-danger': !field.value}" />
+        <span class="pl-2" v-if="label != null">{{ label }}</span>
     </div>
 </template>
 
 <script>
 export default {
     props: ['resourceName', 'field'],
+
+    computed: {
+
+        trueLabel(){
+            return (this.field.true_label != undefined) ? this.field.true_label : null
+        },
+
+        falseLabel(){
+            return (this.field.false_label != undefined) ? this.field.false_label : null
+        },
+
+        label() {
+            return this.field.value == true ? this.trueLabel : this.falseLabel
+        },
+    },
 }
 </script>
