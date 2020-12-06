f15c7bf6e825cbf80d38ec6066d27c85045218e3
Bole Chen <avenger@php.net>
2020-12-07 00:12:12 +0800

    Add support to help()
    Close #30

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,5 +1,5 @@
 <template>
-    <default-field :field="field">
+    <default-field :field="field" :errors="errors" :show-help-text="showHelpText">
         <template slot="field">
             <div class="py-2">
                 <toggle-button
